0d1fd8d19f3e96d701fde876f850c985a4a8245f
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2024-11-17 12:03:01 -0800

    changes done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -15,8 +15,16 @@ export default function Footer() {
         </div>
       
     </div>
-<footer className='container-fluid bg-success py-5 text-light'>
-<div className="container py-3"></div>
+<footer className='container-fluid bg-success pt-5 text-light'>
+<div className="container py-3">
+  <h1 className="text-center text-warning font-pacifico">MyCart</h1>
+  <div className="d-flex align-items-center my-5 justify-content-center ">
+    <a href="" className='d-flex mx-3 align-items-center text-decoration-none justify-content-center bg-light text-success rounded-circle  fs-3' style={{width:'50px',height:'50px'}}><i class="fa-brands fa-facebook"></i></a>
+    <a href="" className='d-flex mx-3 align-items-center text-decoration-none justify-content-center bg-light text-success rounded-circle  fs-3' style={{width:'50px',height:'50px'}}><i class="fa-brands fa-instagram"></i></a>
+    <a href="" className='d-flex mx-3 align-items-center text-decoration-none justify-content-center bg-light text-success rounded-circle  fs-3' style={{width:'50px',height:'50px'}}><i class="fa-brands fa-twitter"></i></a>
+  </div>
+</div>
+<p className='text-center m-0 py-2' style={{fontSize:'15px'}}>All Copyright Reserved &copy; By <span className='font-pacifico text-warning'>MyCart</span></p>
 </footer>
     </>
 
